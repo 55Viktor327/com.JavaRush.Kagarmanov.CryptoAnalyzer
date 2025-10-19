ceaa93e7588779e5c069fee4680baa8e767f057c
Viktor Kagarmanov. tel: +7(934) 023 66 22 <svb28rus@yandex.ru>
2025-10-19 10:59:54 +0300

    First commit

---
+++ src/main/java/org/example/CeasarsCipher.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class CeasarsCipher {
+    private static final char[] ALPHABET = "ёйцукенгшщзхъфывапролджэячсмитьбю .,”’:-!?".toCharArray();
+
+    public static String encryption(String text, int shift) {
+        return transformedText(text, shift);
+    }
+    public static String decrypt(String encryptedText, int shift) {
+
+        return transformedText(encryptedText, -shift);
+    }
+
+    private static String transformedText(String text, int shift) {
+
+        StringBuilder result = new StringBuilder();
+
+        for(char character : text.toCharArray()) {
+            int originalPosition = findCharacterIndex(character);
+            if (originalPosition != -1) {
+                int newPosition = originalPosition + shift;
+                if (newPosition >= ALPHABET.length) {
+                    newPosition = (originalPosition + shift) % ALPHABET.length;
+                    char newChar = ALPHABET[newPosition];
+                    result.append(newChar);
+                }
+            }else{
+                result.append(character);
+            }
+        }
+        return result.toString();
+    }
+
+    private static int findCharacterIndex(char letter) {
+        for (int i = 0; i < ALPHABET.length; i++) {
+            if (letter == ALPHABET[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
