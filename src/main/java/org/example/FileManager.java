144f6018398d72654a3f9fa4e8c918b00b1b61ca
Viktor Kagarmanov. tel: +7(934) 023 66 22 <svb28rus@yandex.ru>
2025-10-20 20:28:29 +0300

    Third commit

--- src/main/java/org/example/FileManager.java
+++ src/main/java/org/example/FileManager.java
@@ -1,10 +1,10 @@
 package org.example;
 
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileManager {
 
@@ -26,9 +26,19 @@ public class FileManager {
         return new String(bytes, StandardCharsets.UTF_8);
     }
 
-    public void writeFile(String content, String filePath) {
+    public void writeFile(String content, String directory, String filename) {
+        try {
+            // Создаем полный путь к файлу, объединяя каталог и имя файла
+            Path path = Paths.get(directory).resolve(filename);
 
+            Files.createDirectories(path.getParent());
+            Files.createFile(path);
 
+            // Записываем содержимое в файл
+            Files.writeString(path, content);
+        } catch (IOException e) {
+            System.err.println("Ошибка при работе с файлами: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
-
 }
